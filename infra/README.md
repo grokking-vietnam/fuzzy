a5568d279a32f49b76281f1cab38a2a7bdd0ea0c
tqtensor <victortang212@gmail.com>
2023-02-27 08:46:52 +0700

    feat: wip - using Packer to create VM image

--- infra/README.md
+++ infra/README.md
@@ -1,7 +1,7 @@
 # Terraform on Proxmox
 
 ## Credentials for Proxmox integration
-Create file `infra/credentials.auto.tfvars`
+Create files `infra/credentials.auto.tfvars` and `infra/packer/credentials.pkr.hcl`
 
 ```txt
 proxmox_api_url = "https://proxmox-server.com:8006/api2/json"
@@ -9,24 +9,14 @@ proxmox_api_token_id = "root@pam!terraform"
 proxmox_api_token_secret = ""
 ```
 
-## Prepare Ubuntu Cloud Init image
-https://austinsnerdythings.com/2021/08/30/how-to-create-a-proxmox-ubuntu-cloud-init-image/
+## Prepare Ubuntu Packer image
+https://www.youtube.com/watch?v=1nf3WOEFq1Y
 
 ```bash
-export distro=focal # Ubuntu Server 20.04 LTS
-wget "https://cloud-images.ubuntu.com/${distro}/current/${distro}-server-cloudimg-amd64.img"
-
-apt update -y && apt install libguestfs-tools -y
-virt-customize -a ${distro}-server-cloudimg-amd64.img --update
-virt-customize -a ${distro}-server-cloudimg-amd64.img --install qemu-guest-agent,wget,curl,telnet,unzip
-
-qm create 9000 --name "ubuntu-${distro}-cloudinit-template" --memory 2048 --cores 2 --net0 virtio,bridge=vmbr1
-qm importdisk 9000 ${distro}-server-cloudimg-amd64.img local-lvm
-qm set 9000 --scsihw virtio-scsi-pci --scsi0 local-lvm:vm-9000-disk-0
-qm set 9000 --boot c --bootdisk scsi0
-qm set 9000 --ide2 local-lvm:cloudinit
-qm set 9000 --serial0 socket --vga serial0
-qm set 9000 --agent enabled=1
-
-qm template 9000
+cd infra/packer/ubuntu-server-focal-docker
+packer validate -var-file='../credentials.pkr.hcl' ubuntu-server-focal-docker.pkr.hcl
+packer build -var-file='../credentials.pkr.hcl' ubuntu-server-focal-docker.pkr.hcl
 ```
+
+## Notes
+1. Replace the domain to your Proxmox server by its explicit IP. `proxmox-server.com` -> `123.45.67.89`
--- infra/deeplab-demo.tf
+++ infra/deeplab-demo.tf
@@ -1,33 +1,33 @@
 resource "proxmox_vm_qemu" "deeplab-demo" {
-    name = "deeplab-demo"
-    desc = "GPU Ubuntu Server"
-    vmid = "401"
-    target_node = "hpz440"
+  name        = "deeplab-demo"
+  desc        = "GPU Ubuntu Server"
+  vmid        = "401"
+  target_node = "hpz440"
 
-    agent = 1
+  agent = 1
 
-    clone = "ubuntu-focal-cloudinit-template"
-    cores = 2
-    sockets = 1
-    cpu = "host"
-    memory = 2048
+  clone   = "ubuntu-server-focal-docker"
+  cores   = 2
+  sockets = 1
+  cpu     = "host"
+  memory  = 2048
 
-    network {
-        bridge = "vmbr1"
-        model = "virtio"
-    }
+  network {
+    bridge = "vmbr1"
+    model  = "virtio"
+  }
 
-    disk {
-        storage = "local-lvm"
-        type = "virtio"
-        size = "20G"
-    }
+  disk {
+    storage = "local-lvm"
+    type    = "virtio"
+    size    = "20G"
+  }
 
-    os_type = "cloud-init"
-    ipconfig0 = "ip=11.11.1.88/16,gw=11.11.1.1"
-    nameserver = "1.1.1.1"
-    ciuser = "gpu"
-    sshkeys = <<EOF
+  os_type    = "cloud-init"
+  ipconfig0  = "ip=11.11.1.86/255.255.255.0,gw=11.11.1.1"
+  nameserver = "1.1.1.1"
+  ciuser     = "tqtensor"
+  sshkeys    = <<EOF
     ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQC0Ji3n1/qwVydGS8OoOpw5LDd0VOcUPPDhRSDmU4Rl3bMceK17jXBZBZz1dofDIJiOKqGRGbYcLxh0kha3epIGyia9Z+z0ZHQg0ByRzuNlpepZYUva5EssOjVekRZtC1NiROS1BREVPG1sSwORdW0KiVJ4UiT7eJRNZwAm/0TZWmLTYdLqMI4FoBp8bzCQPMUbPb27sU0xZB/m8P7xJaAj9dZfpQEvV8azd2cLKNX9R1Fohft0eFE7U3D9oO26CbbQRlAOm9lxSAON9KZp6aAS0I2eY44vBZiprGafl12C/zFdywhJ9D4VcURfncufLUeSAImklz96IL3OK4hurKYhMTGydb9HHN03zcw69gwWAqnVJn3Yx/NflLZE9DIvQzAZQiYu14r5CL2SWNwoS7ui9Y0mdjP3Ah4rwRIjq/37C92g/qph4kqI8XlRe5NgYwdwtEr8vkL3dT0RatJU+ZTIW/uXsAk4rQ1JQ+VplLucEGjzmLZtC/R4q/IaBmGmCOs= tqtensor
     EOF
 }
