54c7b5f3516541cd6fe24bf09464ed43d6b9fa64
tqtensor <victortang212@gmail.com>
2023-03-02 10:06:18 +0000

    chore: add Docker compose

--- infra/deeplap.tf
+++ infra/deeplap.tf
@@ -48,10 +48,11 @@ resource "proxmox_vm_qemu" "deeplab" {
       "curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg",
       "echo \"deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable\" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null",
       "sudo apt-get update",
-      "sleep 15",
+      "sleep 60",
       "sudo apt-get -y install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin",
       "sudo usermod --append --groups docker \"$USER\"",
       "sudo systemctl enable docker",
+      "sudo apt-get -qq -y install docker-compose-plugin",
       "printf '\nDocker installed successfully\n\n'",
       "printf 'Waiting for Docker to start...\n\n'",
       "sleep 5"
